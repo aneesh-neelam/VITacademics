0d969be935f8bec48230b502678c5de07412f84b
Aneesh Neelam <neelam.aneesh@gmail.com>
2014-07-01 19:46:24 +0530

    Repository & Dependency Changes Changed repository name Upgraded dependencies

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
 
 var routes = require('./routes/web/index');
 
@@ -21,12 +21,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(favicon());
+
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({
+                                  extended: true
+                              }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', routes);
 
@@ -37,6 +41,7 @@ app.use('/api/vellore/data', api_vellore_data);
 app.use('/api/chennai', api_chennai);
 app.use('/api/chennai/login', api_chennai_login);
 app.use('/api/chennai/data', api_chennai_data);
+
 
 /// catch 404 and forward to error handler
 app.use(function (req, res, next)
