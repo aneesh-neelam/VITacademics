829838af29bceb37a04ccc9a90cf99bbc6e8f671
Aneesh Neelam <neelam.aneesh@gmail.com>
2014-07-09 15:17:03 +0530

    MIscellaneous changes
    Updated app.js
    Updated views
    Added ignore to bower.json
    Added COOKIE_SECRET for cookie-parser
    Updated README.md
    Updated LICENSE and added License information

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+/*
+ *  VITacademics
+ *  Copyright (C) 2014  Aneesh Neelam <neelam.aneesh@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
@@ -17,33 +35,31 @@ var api_chennai_data = require('./routes/api/chennai/data.js');
 
 var app = express();
 
+app.use(logger('dev'));
+
+app.set('title', 'VITacademics');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-                                  extended: true
-                              }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}));
 
+var cookieSecret = process.env.COOKIE_SECRET || 'randomsecretstring';
+app.use(cookieParser(cookieSecret, {signed: true}));
 
 app.use('/', routes);
-
 app.use('/api/vellore', api_vellore);
 app.use('/api/vellore/login', api_vellore_login);
 app.use('/api/vellore/data', api_vellore_data);
-
 app.use('/api/chennai', api_chennai);
 app.use('/api/chennai/login', api_chennai_login);
 app.use('/api/chennai/data', api_chennai_data);
 
-
-/// catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use(function (req, res, next)
         {
             var err = new Error('Not Found');
@@ -51,10 +67,8 @@ app.use(function (req, res, next)
             next(err);
         });
 
-/// error handlers
-
-// development error handler
-// will print stacktrace
+// error handlers
+// development error handler, will print stacktrace
 if (app.get('env') === 'development')
 {
     app.use(function (err, req, res, next)
@@ -67,8 +81,7 @@ if (app.get('env') === 'development')
             });
 }
 
-// production error handler
-// no stacktraces leaked to user
+// production error handler, no stacktraces leaked to user
 app.use(function (err, req, res, next)
         {
             res.status(err.status || 500);
--- routes/web/index.js
+++ routes/web/index.js
@@ -1,18 +1,42 @@
+/*
+ *  VITacademics
+ *  Copyright (C) 2014  Aneesh Neelam <neelam.aneesh@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function (req, res)
 {
-    res.render('index', { title: 'Express' });
+    res.render('index', { repository: 'https://github.com/aneesh-neelam/VITacademics'});
 });
 
 router.get('/status', function (req, res)
 {
-    var engine = "Heroku";
-    var serverStatus = 'OK';
-    var lastUpdated = '1st July 2014';
-    res.render('status', {engine: engine, serverStatus: serverStatus, lastUpdated: lastUpdated});
+    var port = process.env.PORT || 3000;
+    var status = {
+        host: process.env.HOST || 'localhost',
+        hostname: process.env.HOSTNAME || 'http://localhost:' + port + '/',
+        engine: process.version,
+        platform: process.platform,
+        arch: process.arch,
+        serverStatus: 'OK',
+        lastUpdated: '9th July 2014'
+    }
+    res.render('status', status);
 });
 
 router.get('/api', function (req, res)
