45d5165f8dd200a663de9978f403fe118d6b3463
Aneesh Neelam <neelam.aneesh@gmail.com>
2014-07-19 12:06:34 +0530

    Scraping Marks in Progress
    Scraping marks for CBL, Lab and Project is done
    Not tested though
    
    Scraping PBL marks is in Progress

--- api/vellore/scraper/marks.js
+++ api/vellore/scraper/marks.js
@@ -25,7 +25,94 @@ var unirest = require('unirest');
 exports.scrapeMarks = function (RegNo, sem, callback)
 {
     var marksUri = 'https://academics.vit.ac.in/parent/marks.asp?sem=' + sem;
-    // TODO Scrape Marks
+    var CookieJar = unirest.jar();
+    var myCookie = cache.get(RegNo);
+    var cookieSerial = cookie.serialize(myCookie[0], myCookie[1]);
+
+    var onRequest = function (response)
+    {
+        if (response.error) callback(false, [
+            {Error: errors.codes.Down}
+        ]);
+        else
+        {
+            var marks = [];
+            try
+            {
+                var scraper = cheerio.load(response.body);
+                var PBL = false;
+                var scraperPBL;
+                if (scraper('table table').length > 1)
+                {
+                    PBL = true;
+                    scraperPBL = cheerio.load(scraper('table table').eq(1).html());
+                }
+                scraper = cheerio.load(scraper('table table').eq(0).html());
+                var onEach = function (i, elem)
+                {
+                    var $ = cheerio.load(scraper(this).html());
+                    if (i > 1)
+                    {
+                        var length = $('td').length;
+                        var classnbr = $('td').eq(1).text();
+                        if (length == 18)
+                        {
+                            marks.push({
+                                           'Class Number': classnbr,
+                                           'Course Code': $('td').eq(2).text(),
+                                           'Course Title': $('td').eq(3).text(),
+                                           'Course Type': $('td').eq(4).text(),
+                                           'CAT I': $('td').eq(6).text(),
+                                           'CAT II': $('td').eq(8).text(),
+                                           'Quiz I': $('td').eq(10).text(),
+                                           'Quiz II': $('td').eq(12).text(),
+                                           'Quiz III': $('td').eq(14).text(),
+                                           'Assignment': $('td').eq(16).text(),
+                                           'Type': 'CBL'
+                                       });
+                        }
+                        else if (length == 8)
+                        {
+                            marks.push({
+                                           'Class Number': classnbr,
+                                           'Course Code': $('td').eq(2).text(),
+                                           'Course Title': $('td').eq(3).text(),
+                                           'Lab CAM': $('td').eq(7).text(),
+                                           'Type': 'Lab'
+                                       });
+                        }
+                        else if (length == 6)
+                        {
+                            marks.push({
+                                           'Class Number': classnbr,
+                                           'Course Code': $('td').eq(2).text(),
+                                           'Course Title': $('td').eq(3).text(),
+                                           'Type': 'Project'
+                                       });
+                        }
+                    }
+                };
+                scraper('tr').each(onEach);
+                var pblMarks = [];
+                if (PBL)
+                {
+                    var onEachPBL = function (i, elem)
+                    {
+                        var row = i % 8;
+                    };
+                    scraperPBL('tr').each(onEachPBL);
+                }
+            }
+
+        }
+    };
+
+
+    CookieJar.add(unirest.cookie(cookieSerial), marksUri);
+    unirest.post(timetableUri)
+        .jar(CookieJar)
+        .end(onRequest);
+
     var marks = [
         {Error: errors.codes.ToDo}
     ];
