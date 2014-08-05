565c13cc1b75f66d03dd5e296ddbf799a3ba7205
Aneesh Neelam <neelam.aneesh@gmail.com>
2014-08-05 20:53:26 +0530

    Token Generation Resources Added Added Characters for Token Generation Updated Dependencies

--- api/vellore/friends/resource.js
+++ api/vellore/friends/resource.js
@@ -16,3 +16,43 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+var resources = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9'
+];
+
+module.exports.resources = resources;
