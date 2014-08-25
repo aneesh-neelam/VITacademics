4251b637fcb743ac38b970d8549834f1e833365b
Aneesh Neelam <neelam.aneesh@gmail.com>
2014-08-25 20:42:18 +0530

    Updated /status

--- routes/web/index.js
+++ routes/web/index.js
@@ -35,7 +35,7 @@ router.get('/status', function (req, res)
         platform: process.platform,
         arch: process.arch,
         serverStatus: 'OK',
-        lastUpdated: '20th July 2014'
+        lastUpdated: '25th August 2014'
     };
     res.render('status', status);
 });
