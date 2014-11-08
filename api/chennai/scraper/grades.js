a968f63fe816c681cd395393b6090d3a29d71ba5
aneesh-neelam <neelam.aneesh@gmail.com>
2014-11-08 22:52:51 +0530

    Initial Grade Support Added Routes Added API functions
    TODO Scrape

---
+++ api/chennai/scraper/grades.js
@@ -0,0 +1,53 @@
+/*
+ *  VITacademics
+ *  Copyright (C) 2014  Aneesh Neelam <neelam.aneesh@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var cache = require('memory-cache');
+var cheerio = require('cheerio');
+var cookie = require('cookie');
+var path = require('path');
+var unirest = require('unirest');
+
+var errors = require(path.join(__dirname, '..', '..', 'error'));
+
+exports.scrapeGrades = function (RegNo, sem, callback) {
+    var data = {RegNo: RegNo};
+    if (cache.get(RegNo) !== null) {
+        var Sem = sem || process.env.CHENNAI_PREVIOUS_SEMESTER || 'WS';
+        var timetableUri = 'http://27.251.102.132/parent/grade.asp?sem=' + Sem;
+        var CookieJar = unirest.jar();
+        var myCookie = cache.get(RegNo);
+        var cookieSerial = cookie.serialize(myCookie[0], myCookie[1]);
+        var onRequest = function (response) {
+            if (response.error) {
+                callback(true, {Error: errors.codes.Down});
+            }
+            else {
+                // TODO Grades
+                callback(null, 'Nothing');
+            }
+        };
+        CookieJar.add(unirest.cookie(cookieSerial), timetableUri);
+        unirest.post(timetableUri)
+            .jar(CookieJar)
+            .end(onRequest);
+    }
+    else {
+        data.Error = errors.codes.TimedOut;
+        callback(true, data);
+    }
+};
--- routes/api/vellore/data.js
+++ routes/api/vellore/data.js
@@ -21,6 +21,7 @@ var path = require('path');
 var router = express.Router();
 
 var api_data = require(path.join(__dirname, '..', '..', '..', 'api', 'vellore', 'scraper', 'aggregate'));
+var api_grades = require(path.join(__dirname, '..', '..', '..', 'api', 'vellore', 'scraper', 'grades'));
 
 
 router.get('/refresh', function (req, res) {
@@ -37,6 +38,15 @@ router.get('/first', function (req, res) {
         res.send(data);
     };
     api_data.getData(RegNo, true, onGetData)
+});
+
+router.get('/grades', function (req, res) {
+    var RegNo = req.query.regno;
+    var Sem = req.query.sem; // FS || WS
+    var onGetData = function (err, data) {
+        res.send(data);
+    };
+    api_grades.scrapeGrades(RegNo, Sem, onGetData)
 });
 
 module.exports = router;
