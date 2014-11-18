f833b9ffd8eec56821aa5b0df18199752ad064f8
aneesh-neelam <neelam.aneesh@gmail.com>
2014-11-18 22:19:13 +0530

    Fixed Authentication for Grades API Now DoB for that as well

--- api/vellore/login/captcha-parser.js
+++ api/vellore/login/captcha-parser.js
@@ -17,6 +17,8 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+var path = require('path');
+
 var captchaResource = require(path.join(__dirname, 'captcha-resource'));
 
 
