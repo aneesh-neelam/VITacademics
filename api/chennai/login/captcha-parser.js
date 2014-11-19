c187dee63eb7b52a1289f9d78fe14c25c31d85bf
aneesh-neelam <neelam.aneesh@gmail.com>
2014-11-19 18:29:50 +0530

    Refactored Captcha Parsing Added functionality to Chennai campus Now works for both campuses

--- api/chennai/login/captcha-parser.js
+++ api/chennai/login/captcha-parser.js
@@ -17,10 +17,14 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+var path = require('path');
+
+var captchaResource = require(path.join(__dirname, 'captcha-resource'));
+
+
 var parseBuffer = function (bitmapBuffer) {
     var pixelMap = getPixelMapFromBuffer(bitmapBuffer);
-    // TODO Parse Captcha
-    return '123456';
+    return getCaptcha(pixelMap);
 };
 
 var getPixelMapFromBuffer = function (bitmapBuffer) {
@@ -36,7 +40,130 @@ var getPixelMapFromBuffer = function (bitmapBuffer) {
         subArray.push(bitmapBuffer.readUInt8(i));
     }
     pixelMap.push(subArray);
+    pixelMap.reverse();
     return pixelMap;
+};
+
+var getCaptcha = function (img) {
+    var order = captchaResource.keyOrder;
+    var keys = captchaResource.keyMask;
+
+    function matchImg(rx, ry, pix, mask) {
+        var flag = 1;
+        var breakflag = 0;
+        var count = 0;
+        for (var x = 0; x < mask.length; ++x) {
+            for (var y = 0; y < mask[0].length; ++y) {
+                try {
+                    if (mask[x][y] == '1') {
+                        if (pix[rx + x][ry + y] == '1') {
+                            count += 1;
+                        }
+                        else {
+                            flag = 0;
+                            breakflag = 1;
+                            break;
+                        }
+                    }
+                }
+                catch (e) {
+                    flag = 0;
+                    breakflag = 1;
+                    break;
+                }
+            }
+            if (breakflag) {
+                break;
+            }
+        }
+        if (count === 0) {
+            flag = 0;
+        }
+        return flag;
+    }
+
+    var skip = function (start, end, y) {
+        var flag = 0;
+        for (var i = 0; i < start.length; ++i) {
+            if (y >= start[i] && y <= end[i]) {
+                flag = 1;
+                break;
+            }
+        }
+        return flag;
+    };
+
+    var sort = function (sorter, captcha) {
+        for (var i = 0; i < sorter.length; ++i) {
+            var less = sorter[i];
+            var swap = 0;
+            var ls = i;
+            for (var k = i; k < sorter.length; k++) {
+                if (sorter[k] < less) {
+                    less = sorter[k];
+                    ls = k;
+                    swap = 1;
+                }
+            }
+            if (swap) {
+                var temps = sorter[i];
+                sorter[i] = sorter[ls];
+                sorter[ls] = temps;
+                var tempc = captcha[i];
+                captcha[i] = captcha[ls];
+                captcha[ls] = tempc;
+            }
+        }
+    };
+
+    var temp = 0;
+    var x, y;
+    for (x = 0; x < 25; ++x) {
+        for (y = 0; y < 132; ++y) {
+            temp = img[x][y];
+            if (x !== 0 && x !== 24) {
+                if (img[x + 1][y] === 0 && temp === 1 && img[x - 1][y] === 0) {
+                    img[x][y] = 0;
+                }
+            }
+
+        }
+    }
+    var yoff = 2;
+    var xoff = 2;
+    var skipstart = [];
+    var skipend = [];
+    var sorter = [];
+    var captcha = [];
+    for (var l = 0; l < 36; ++l) {
+        var mask = keys[order[l]];
+        var f = 0;
+        for (x = xoff; x < 25; ++x) {
+            for (y = yoff; y < 132; ++y) {
+                if (skip(skipstart, skipend, y)) {
+                    continue;
+                }
+                else {
+                    if (matchImg(x, y, img, mask)) {
+                        skipstart.push(y);
+                        skipend.push(y + mask[0].length);
+                        sorter.push(y);
+                        captcha.push(order[l]);
+                        f = f + 1;
+                    }
+                }
+            }
+        }
+        if (f == 6) {
+            break;
+        }
+    }
+    sort(sorter, captcha);
+    var res = "";
+    for (var i = 0; i < captcha.length; ++i) {
+        res = res + captcha[i];
+    }
+    return res;
 };
 
 module.exports.parseBuffer = parseBuffer;
