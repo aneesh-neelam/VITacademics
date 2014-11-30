9be3a8ded7e48c20b07dc1941ef2c068cb9d2d16
aneesh-neelam <neelam.aneesh@gmail.com>
2014-11-30 15:13:49 +0530

    Trying out TxtWeb

---
+++ routes/api/txtWeb.js
@@ -0,0 +1,34 @@
+/*
+ *  VITacademics
+ *  Copyright (C) 2014  Aneesh Neelam <neelam.aneesh@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/', function (req, res) {
+    if (res.query['txtweb-message'] && res.query['txtweb-mobile']) {
+        console.log(res.query['txtweb-message']);
+        console.log(res.query['txtweb-mobile']);
+    }
+    else {
+        var GoogleAnalytics = process.env.GOOGLE_ANALYTICS || 'UA-35429946-2';
+        res.render('txtweb', {GoogleAnalytics: GoogleAnalytics});
+    }
+});
+
+module.exports = router;
