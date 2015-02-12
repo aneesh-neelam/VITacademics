28d9a346e51cc17ececb502d2123b6fcfc424104
aneesh-neelam <neelam.aneesh@gmail.com>
2015-02-12 16:19:11 +0530

    Revert "Added Test for Memory Leak"
    This reverts commit ce851ccd78cbb485261fa48700b53264d6854345.
    Because it doesn't matter anymore

--- test/rabbitmq-test.js
+++ test/rabbitmq-test.js
@@ -1,40 +1,7 @@
-#!/usr/bin/env node
-
-/*
- *  VITacademics
- *  Copyright (C) 2015  Aneesh Neelam <neelam.aneesh@gmail.com>
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 var jackrabbit = require('jackrabbit');
 
 var amqpUri = process.env.AMQP_URI || 'amqp://localhost';
 var queue = jackrabbit(amqpUri);
-
-var newrelic;
-if (process.env.NEWRELIC_APP_NAME && process.env.NEWRELIC_LICENSE) {
-    newrelic = require('newrelic');
-}
-
-var log;
-if (process.env.LOGENTRIES_TOKEN) {
-    var logentries = require('node-logentries');
-    log = logentries.logger({
-        token: process.env.LOGENTRIES_TOKEN
-    });
-}
 
 queue.on('connected', function () {
     var name = 'VITacademics';
