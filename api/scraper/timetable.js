8cd9d0d44de79a26d81d0823cd42c1fea5c758a2
ayushagarwal95 <agarwalayush161@gmail.com>
2015-02-15 23:26:01 +0530

    Timings minor bug fix for slots L5+L6

--- api/scraper/timetable.js
+++ api/scraper/timetable.js
@@ -215,16 +215,22 @@ exports.scrapeTimetable = function (app, data, callback) {
                                 }
                                 else {
                                     if (isEndTime) {
-                                        time.setMinutes(30);
-                                    }
-                                    else {
-                                        time.setMinutes(50);
-                                    }
-                                    if (isEndTime) {
                                         time.setHours(morningStartHour + column);
+                                        if((column % 6) === 5 ) {
+                                          time.setMinutes(30);
+                                        }
+                                        else {
+                                          time.setMinutes(40);
+                                        }
                                     }
                                     else {
                                         time.setHours(morningStartHour + column - 1);
+                                        if((column % 6) === 4) {
+                                          time.setMinutes(50);
+                                        }
+                                        else {
+                                          time.setMinutes(40);
+                                        }
                                     }
                                 }
                             }
