d63853177e8be73f59ed76023627a86f79cbeccc
aneesh-neelam <neelam.aneesh@gmail.com>
2015-03-26 00:20:08 +0530

    Inital Node.js/io.js Project
    * Created package.json
    * Created Heroku Procfile
    * Added .gitattributes, .editorconfig
    * Modified .gitignore, LICENSE, README.md
    * Added bin/worker
    * Added newrelic.js for future use

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # VITacademics-Worker
-The Background Worker for the VITacademics Backend
+
+The background worker for the VITacademics HTTP API Server
+
+Please report any bugs or issues [here](https://github.com/aneesh-neelam/VITacademics-Worker/issues)
+
+## Instructions for Installation:
+### Install Node.js/io.js (Latest Stable Release)
+### Install all dependencies
+
+```
+$ npm install
+```
+
+### Run the server locally
+
+```
+$ npm start
+```
+
+## External Requirements:
+- MongoDB
+- RabbitMQ
---
+++ newrelic.js
@@ -0,0 +1,41 @@
+/*
+ *  VITacademics - Worker
+ *  Copyright (C) 2015  Aneesh Neelam <neelam.aneesh@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+'use strict';
+
+var log;
+if (process.env.LOGENTRIES_TOKEN) {
+  var logentries = require('node-logentries');
+  log = logentries.logger({
+    token: process.env.LOGENTRIES_TOKEN
+  });
+}
+
+if (process.env.NEWRELIC_APP_NAME && process.env.NEWRELIC_LICENSE) {
+  var app_name = process.env.NEWRELIC_APP_NAME;
+  var license = process.env.NEWRELIC_LICENSE;
+}
+
+exports.config = {
+  app_name: [app_name],
+  license_key: license,
+  logging: {
+    level: 'info'
+  },
+  capture_params: true
+};
