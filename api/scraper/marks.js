78ca5cf58380a6e9cfefec3d1b1892a6479cb850
Saurabh Mathur <saurabhmathur96@gmail.com>
2017-02-07 21:57:48 +0530

    marks scraper: kintsugi

--- api/scraper/marks.js
+++ api/scraper/marks.js
@@ -49,244 +49,39 @@ exports.scrapeMarks = function (app, data, callback) {
     else {
       let marks = [];
       try {
-        let scraper = cheerio.load(response.body);
-        let PBL = false;
-        let scraperPBL = null;
-        let CBL = true;
-        if (scraper('table table').length > 1) {
-          PBL = true;
-          scraperPBL = cheerio.load(scraper('table table').eq(1).html());
-        }
-        else if (scraper('u').eq(1).text().substring(0, 3) === 'PBL') {
-          PBL = true;
-          scraperPBL = cheerio.load(scraper('table table').eq(0).html());
-          CBL = false;
-        }
-        scraper = cheerio.load(scraper('table table').eq(0).html());
-        if (CBL) {
-          const onEach = function (i, elem) {
-            const htmlColumn = cheerio.load(scraper(this).html())('td');
-            if (i > 1) {
-              const length = htmlColumn.length;
-              const classnbr = parseInt(htmlColumn.eq(1).text());
-              if (parseInt(data.reg_no.slice(0, 2)) < 15) {
-                if (length == 18) {
-                  marks.push({
-                    class_number: classnbr,
-                    course_code: htmlColumn.eq(2).text(),
-                    course_title: htmlColumn.eq(3).text(),
-                    course_type: htmlColumn.eq(4).text(),
-                    assessments: [
-                      {
-                        title: "CAT-I",
-                        max_marks: 50,
-                        weightage: 15,
-                        conducted_on: "Check Exam Schedule",
-                        status: htmlColumn.eq(5).text(),
-                        scored_marks: parseFloat(htmlColumn.eq(6).text()),
-                        scored_percentage: Math.round(parseFloat(htmlColumn.eq(6).text()) / 50 * 15 * 1e2) / 1e2
-                      },
-                      {
-                        title: "CAT-II",
-                        max_marks: 50,
-                        weightage: 15,
-                        conducted_on: "Check Exam Schedule",
-                        status: htmlColumn.eq(7).text(),
-                        scored_marks: parseFloat(htmlColumn.eq(8).text()),
-                        scored_percentage: Math.round(parseFloat(htmlColumn.eq(8).text()) / 50 * 15 * 1e2) / 1e2
-                      },
-                      {
-                        title: "Quiz-I",
-                        max_marks: 5,
-                        weightage: 5,
-                        conducted_on: "Tentative, set by course faculty",
-                        status: htmlColumn.eq(9).text(),
-                        scored_marks: parseFloat(htmlColumn.eq(10).text()),
-                        scored_percentage: parseFloat(htmlColumn.eq(10).text())
-                      },
-                      {
-                        title: "Quiz-II",
-                        max_marks: 5,
-                        weightage: 5,
-                        conducted_on: "Tentative, set by course faculty",
-                        status: htmlColumn.eq(11).text(),
-                        scored_marks: parseFloat(htmlColumn.eq(12).text()),
-                        scored_percentage: parseFloat(htmlColumn.eq(12).text())
-                      },
-                      {
-                        title: "Quiz-III",
-                        max_marks: 5,
-                        weightage: 5,
-                        conducted_on: "Tentative, set by course faculty",
-                        status: htmlColumn.eq(13).text(),
-                        scored_marks: parseFloat(htmlColumn.eq(14).text()),
-                        scored_percentage: parseFloat(htmlColumn.eq(14).text())
-                      },
-                      {
-                        title: "Assignment",
-                        max_marks: 5,
-                        weightage: 5,
-                        conducted_on: "Tentative, set by course faculty",
-                        status: htmlColumn.eq(15).text(),
-                        scored_marks: parseFloat(htmlColumn.eq(16).text()),
-                        scored_percentage: parseFloat(htmlColumn.eq(16).text())
-                      }
-                    ]
-                  });
-                }
-                else if (length == 8) {
-                  marks.push({
-                    class_number: classnbr,
-                    course_code: htmlColumn.eq(2).text(),
-                    course_title: htmlColumn.eq(3).text(),
-                    course_type: htmlColumn.eq(4).text(),
-                    assessments: [
-                      {
-                        title: "Lab CAM",
-                        max_marks: 50,
-                        weightage: 50,
-                        conducted_on: "Tentative, set by lab faculty",
-                        status: htmlColumn.eq(6).text(),
-                        scored_marks: parseFloat(htmlColumn.eq(7).text()),
-                        scored_percentage: parseFloat(htmlColumn.eq(7).text())
-                      }
-                    ]
-                  });
-                }
-                else if (length == 6) {
-                  marks.push({
-                    class_number: classnbr,
-                    course_code: htmlColumn.eq(2).text(),
-                    course_title: htmlColumn.eq(3).text(),
-                    course_type: htmlColumn.eq(4).text(),
-                    assessments: []
-                  });
-                }
-              }
-              else {
-                if (length == 10) {
-                  marks.push({
-                    class_number: classnbr,
-                    course_code: htmlColumn.eq(2).text(),
-                    course_title: htmlColumn.eq(3).text(),
-                    course_type: htmlColumn.eq(4).text(),
-                    assessments: [
-                      {
-                        title: "CAT-I",
-                        max_marks: 50,
-                        weightage: 10,
-                        conducted_on: "Check Exam Schedule",
-                        status: htmlColumn.eq(5).text(),
-                        scored_marks: parseFloat(htmlColumn.eq(6).text()),
-                        scored_percentage: Math.round(parseFloat(htmlColumn.eq(6).text()) / 50 * 10 * 1e2) / 1e2
-                      },
-                      {
-                        title: "CAT-II",
-                        max_marks: 50,
-                        weightage: 10,
-                        conducted_on: "Check Exam Schedule",
-                        status: htmlColumn.eq(7).text(),
-                        scored_marks: parseFloat(htmlColumn.eq(8).text()),
-                        scored_percentage: Math.round(parseFloat(htmlColumn.eq(8).text()) / 50 * 10 * 1e2) / 1e2
-                      },
-                      {
-                        title: "Digital Assignment",
-                        max_marks: 30,
-                        weightage: 30,
-                        conducted_on: "Tentative, set by faculty",
-                        status: "",
-                        scored_marks: parseFloat(htmlColumn.eq(9).text()),
-                        scored_percentage: parseFloat(htmlColumn.eq(9).text())
-                      }
-                    ]
-                  });
-                }
-                else if (length == 6) {
-                  marks.push({
-                    class_number: classnbr,
-                    course_code: htmlColumn.eq(2).text(),
-                    course_title: htmlColumn.eq(3).text(),
-                    course_type: htmlColumn.eq(4).text(),
-                    assessments: []
-                  });
-                }
-              }
-            }
-          };
-          scraper('tr').each(onEach);
-        }
-        if (PBL) {
-          const pblMarks = [];
-          const onEachPBL = function (i, elem) {
-            const htmlColumn = cheerio.load(scraper(this).html())('td');
-            const j = i - 1;
-            const course = Math.floor(j / 7);
-            const row = j % 7;
-            switch (row) {
-              case 0:
-                pblMarks[course] = {
-                  class_number: parseInt(htmlColumn.eq(1).text()),
-                  course_code: htmlColumn.eq(2).text(),
-                  course_title: htmlColumn.eq(3).text(),
-                  course_type: htmlColumn.eq(4).text(),
-                  assessments: [],
-                  pbl_details: {
-                    1: {title: htmlColumn.eq(6).text()},
-                    2: {title: htmlColumn.eq(7).text()},
-                    3: {title: htmlColumn.eq(8).text()},
-                    4: {title: htmlColumn.eq(9).text()},
-                    5: {title: htmlColumn.eq(10).text()}
-                  }
-                };
-                break;
-              case 1:
-                pblMarks[course].pbl_details[1]['max_marks'] = parseFloat(htmlColumn.eq(1).text());
-                pblMarks[course].pbl_details[2]['max_marks'] = parseFloat(htmlColumn.eq(2).text());
-                pblMarks[course].pbl_details[3]['max_marks'] = parseFloat(htmlColumn.eq(3).text());
-                pblMarks[course].pbl_details[4]['max_marks'] = parseFloat(htmlColumn.eq(4).text());
-                pblMarks[course].pbl_details[5]['max_marks'] = parseFloat(htmlColumn.eq(5).text());
-                break;
-              case 2:
-                pblMarks[course].pbl_details[1]['weightage'] = parseFloat(htmlColumn.eq(1).text());
-                pblMarks[course].pbl_details[2]['weightage'] = parseFloat(htmlColumn.eq(2).text());
-                pblMarks[course].pbl_details[3]['weightage'] = parseFloat(htmlColumn.eq(3).text());
-                pblMarks[course].pbl_details[4]['weightage'] = parseFloat(htmlColumn.eq(4).text());
-                pblMarks[course].pbl_details[5]['weightage'] = parseFloat(htmlColumn.eq(5).text());
-                break;
-              case 3:
-                pblMarks[course].pbl_details[1]['conducted_on'] = moment(htmlColumn.eq(1).text(), 'DD-MMM-YYYY').isValid() ? moment(htmlColumn.eq(1).text(), 'DD-MMM-YYYY').format('YYYY-MM-DD') : null;
-                pblMarks[course].pbl_details[2]['conducted_on'] = moment(htmlColumn.eq(2).text(), 'DD-MMM-YYYY').isValid() ? moment(htmlColumn.eq(2).text(), 'DD-MMM-YYYY').format('YYYY-MM-DD') : null;
-                pblMarks[course].pbl_details[3]['conducted_on'] = moment(htmlColumn.eq(3).text(), 'DD-MMM-YYYY').isValid() ? moment(htmlColumn.eq(3).text(), 'DD-MMM-YYYY').format('YYYY-MM-DD') : null;
-                pblMarks[course].pbl_details[4]['conducted_on'] = moment(htmlColumn.eq(4).text(), 'DD-MMM-YYYY').isValid() ? moment(htmlColumn.eq(4).text(), 'DD-MMM-YYYY').format('YYYY-MM-DD') : null;
-                pblMarks[course].pbl_details[5]['conducted_on'] = moment(htmlColumn.eq(5).text(), 'DD-MMM-YYYY').isValid() ? moment(htmlColumn.eq(5).text(), 'DD-MMM-YYYY').format('YYYY-MM-DD') : null;
-                break;
-              case 4:
-                pblMarks[course].pbl_details[1]['status'] = htmlColumn.eq(1).text();
-                pblMarks[course].pbl_details[2]['status'] = htmlColumn.eq(2).text();
-                pblMarks[course].pbl_details[3]['status'] = htmlColumn.eq(3).text();
-                pblMarks[course].pbl_details[4]['status'] = htmlColumn.eq(4).text();
-                pblMarks[course].pbl_details[5]['status'] = htmlColumn.eq(5).text();
-                break;
-              case 5:
-                pblMarks[course].pbl_details[1]['scored_marks'] = parseFloat(htmlColumn.eq(1).text());
-                pblMarks[course].pbl_details[2]['scored_marks'] = parseFloat(htmlColumn.eq(2).text());
-                pblMarks[course].pbl_details[3]['scored_marks'] = parseFloat(htmlColumn.eq(3).text());
-                pblMarks[course].pbl_details[4]['scored_marks'] = parseFloat(htmlColumn.eq(4).text());
-                pblMarks[course].pbl_details[5]['scored_marks'] = parseFloat(htmlColumn.eq(5).text());
-                break;
-              case 6:
-                pblMarks[course].pbl_details[1]['scored_percentage'] = parseFloat(htmlColumn.eq(1).text());
-                pblMarks[course].pbl_details[2]['scored_percentage'] = parseFloat(htmlColumn.eq(2).text());
-                pblMarks[course].pbl_details[3]['scored_percentage'] = parseFloat(htmlColumn.eq(3).text());
-                pblMarks[course].pbl_details[4]['scored_percentage'] = parseFloat(htmlColumn.eq(4).text());
-                pblMarks[course].pbl_details[5]['scored_percentage'] = parseFloat(htmlColumn.eq(5).text());
-                break;
-            }
-          };
-          scraperPBL('tr').each(onEachPBL);
-          marks = marks.concat(pblMarks);
-        }
+        let $ = cheerio.load(response.body, {
+          normalizeWhitespace: true
+        });
+
+        let marks = $("table").eq(1).find("tr[bgcolor='#EDEADE']").map((i, element) => {
+
+          let course_marks = $(element).next().find("table").find("tr[bgcolor='#CCCCCC']").map((j, e) => {
+
+            let td = $(e).find("td");
+            return {
+              title: td.eq(1).text().trim(),
+              max_marks: parseFloat(td.eq(2).text()),
+              weightage: parseFloat(td.eq(3).text()),
+              conducted_on: "Tentative, set by course faculty",
+              status: td.eq(4).text(),
+              scored_marks: parseFloat(td.eq(5).text()),
+              scored_percentage: parseFloat(td.eq(6).text())
+            };
+
+          }).get();
+
+          let td = $(element).find("td");
+          return {
+            class_number: parseInt(td.eq(1).text()),
+            course_code: td.eq(2).text(),
+            course_title: td.eq(3).text(),
+            course_type: td.eq(6).text(),
+            marks: course_marks
+          }
+
+        }).get();
         callback(null, marks);
+
       }
       catch (ex) {
         data.status = status.dataParsing;
